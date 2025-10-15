056f5cf5873f973f951f2cdb351f789e41ff5376
deathate <ho.aron0105@gmail.com>
2025-10-15 17:53:30 +0800

    Auto commit 2025-10-15 17:53:30

--- Mbffg/src/main.rs
+++ Mbffg/src/main.rs
@@ -130,7 +130,7 @@ fn perform_main_stage(
     display_progress_step(1);
     let mut mbffg = MBFFG::new(file_name, debug_config);
     mbffg.pa_bits_exp = match testcase {
-        "c1_1" => 1.04,
+        "c1_1" => 1.05,
         "c1_2" => 1.05,
         "c2_1" => 0.5,
         "c2_2" => 0.6,
@@ -244,10 +244,10 @@ fn main() {
         // Test different stages of the MBFF optimization pipeline
 
         // Testcase 1
-        // perform_main_stage()
-        //     .testcase("c1_1")
-        //     .current_stage(Stage::Complete)
-        //     .call();
+        perform_main_stage()
+            .testcase("c1_1")
+            .current_stage(Stage::Complete)
+            .call();
 
         // Testcase 1 hidden
         // perform_main_stage()
@@ -256,11 +256,11 @@ fn main() {
         //     .call();
 
         // Testcase 2
-        perform_main_stage()
-            .testcase("c2_1")
-            .current_stage(Stage::Complete)
-            .use_evaluator(true)
-            .call();
+        // perform_main_stage()
+        //     .testcase("c2_1")
+        //     .current_stage(Stage::Complete)
+        //     .use_evaluator(true)
+        //     .call();
 
         // Testcase 2 hidden
         // perform_main_stage()
